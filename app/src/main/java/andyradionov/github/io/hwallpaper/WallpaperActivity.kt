26a4a6706370467bf88cfd237e8c3bd0e694635a
Andrey Radionov <andyomsk@gmail.com>
2018-07-28 15:12:46 +0300

    Initial commit

---
+++ app/src/main/java/andyradionov/github/io/hwallpaper/WallpaperActivity.kt
@@ -0,0 +1,12 @@
+package andyradionov.github.io.hwallpaper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WallpaperActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallpaper)
+    }
+}
---
+++ app/src/main/java/andyradionov/github/io/hwallpaper/app/AppPreferences.kt
@@ -0,0 +1,8 @@
+package andyradionov.github.io.hwallpaper.app
+
+import android.support.constraint.BuildConfig
+
+/**
+ * @author Andrey Radionov
+ */
+const val EXCHANGE_API_KEY = BuildConfig.ApiKey;
